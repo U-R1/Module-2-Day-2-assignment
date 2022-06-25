33c2c3c8cbc3b9fe0587dd1913c15116aad9d891
U-R1 <106154361+U-R1@users.noreply.github.com>
2022-06-25 13:58:42 +0200

    Add files via upload

---
+++ d4.js
@@ -0,0 +1,89 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/* 1. Number:       can be any number
+   2. String:       sequence of characters, can type text with it 
+   3. Boolean:      true or false
+   4. Undefined:    variable which hasn't been assigned any value 
+   5. Null:         can empty the value of a variable*/
+
+/* EXERCISE 2
+ Try to describe what an object is, in your own words.
+*/
+
+/* WRITE YOUR ANSWER HERE 
+Group of variables that describe the same entity*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+ */
+
+/* WRITE YOUR ANSWER HERE */
+ let a = 12
+ let b = 20 
+ result = a+b
+ console.log(a)
+ console.log(b)
+ console.log(result)
+ 
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/ 
+
+/* WRITE YOUR ANSWER HERE*/ 
+let x = 12
+console.log(x)
+
+/* EXERCISE 5
+Create a variable called name and assign to it your name as a string.
+*/
+
+/* WRITE YOUR ANSWER HERE*/ 
+let name = "umar"
+console.log(name)
+
+
+/* EXERCISE 6
+ Execute a subtraction between the number 4 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* WRITE YOUR ANSWER HERE*/
+let w = 4
+result2 = w-x
+console.log(w)
+console.log(w,x)
+console.log(result2) 
+
+/* EXERCISE 7
+Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+ YOU DON'T NEED AN IF/ELSE BLOCK. It is enough to use console.log()
+*/
+
+/* WRITE YOUR ANSWER HERE*/ 
+let name1 = "john"
+let name2 = "John"
+console.log(name1)
+console.log(name2)
+console.log(name1 === name2)
+parseInt(name2)
+console.log(parseInt(name2)=== name1)
+
+
+
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
